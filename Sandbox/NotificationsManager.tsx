7bf5212697804b98d805c2311043945262ec595f
Jay Koutavas <jay@heynow.com>
2023-05-02 10:53:07 -0400

    Displaying a notification when android app is in foreground, using react-native-push-notification

--- Sandbox/NotificationsManager.tsx
+++ Sandbox/NotificationsManager.tsx
@@ -1,55 +1,72 @@
-import { useEffect } from "react";
+import React, {useEffect} from 'react';
 import messaging from '@react-native-firebase/messaging';
-import { Alert, PermissionsAndroid, Platform } from "react-native";
+import {PermissionsAndroid, Platform} from 'react-native';
+import PushNotification from 'react-native-push-notification';
+
+const localNotificationsChannel = 'localNotificationsChannel';
 
 export const NotificationsManager: React.FC = () => {
+  if (Platform.OS === 'android') {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    );
+    // Register background handler
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log(
+        `Message handled in the background! ${JSON.stringify(remoteMessage)}`,
+      );
+    });
 
-    if( Platform.OS === 'android') {
-        PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
-        // Register background handler
-        messaging().setBackgroundMessageHandler(async remoteMessage => {
-          console.log(
-            `Message handled in the background! ${JSON.stringify(remoteMessage)}`,
-          );
-        });
-      }
-      
-      const notificationConfigure = async () => {
-        // check if we have permissions
-        let enabled = await messaging().hasPermission();
-        if (enabled === messaging.AuthorizationStatus.AUTHORIZED) {
-          const fcmToken = await messaging().getToken();
-          if (fcmToken) {
-            console.log(`FCM token ${fcmToken}`);
-          } else {
-            // user doesn't have a device token yet
-            console.warn("no token");
-          }
+    PushNotification.createChannel(
+      {
+        channelId: localNotificationsChannel,
+        channelName: 'Educational Notifications',
+      },
+      () => {},
+    );
+  }
+
+  useEffect(() => {
+    const notificationConfigure = async () => {
+      // check if we have permissions
+      let enabled = await messaging().hasPermission();
+      if (enabled === messaging.AuthorizationStatus.AUTHORIZED) {
+        const fcmToken = await messaging().getToken();
+        if (fcmToken) {
+          console.log(`FCM token ${fcmToken}`);
         } else {
-          await messaging().requestPermission();
-          console.log("permission requested");
-          enabled = await messaging().hasPermission();
-          console.log(`done getting permissions: ${enabled}`);
-          if (!enabled) {
-            return false;
-          }
+          // user doesn't have a device token yet
+          console.warn('no token');
         }
-    
-        return true;
-      };
+      } else {
+        await messaging().requestPermission();
+        console.log('permission requested');
+        enabled = await messaging().hasPermission();
+        console.log(`done getting permissions: ${enabled}`);
+        if (!enabled) {
+          return false;
+        }
+      }
 
-      
-    useEffect(() => {
-      notificationConfigure();
-    }, [notificationConfigure]);
+      return true;
+    };
 
-    useEffect(() => {
-        const unsubscribe = messaging().onMessage(async remoteMessage => {
-          Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-        });
-    
-        return unsubscribe;
-      }, []);
+    notificationConfigure();
+  }, []);
 
-    return <></>;
-}
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log(`got ${remoteMessage.notification?.title}`);
+      PushNotification.localNotification({
+        message: remoteMessage.notification?.body || '',
+        title: remoteMessage.notification?.title,
+        category: localNotificationsChannel,
+        channelId: localNotificationsChannel,
+      });
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return <></>;
+};
