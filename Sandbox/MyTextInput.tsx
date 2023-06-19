4d4a59fb37c9433053a79f63094937bca53a7d19
jay koutavas <jay@heynow.com>
2023-06-19 15:32:32 -0400

    Got rid of the TSC2322 error

--- Sandbox/MyTextInput.tsx
+++ Sandbox/MyTextInput.tsx
@@ -1,18 +1,4 @@
-// @ts-nocheck
-
-/**
- * Using ts-nocheck to avoid this hard to resolve error:
- * error TS2322: Type 'ForwardedRef<TextInput>' is not assignable to type '((((instance: TextInput | null) => void) | RefObject<TextInput>) & (((instance: PropsWithChildren<InputProps> | null) => void) | RefObject<...>)) | null | undefined'.
-  Type '(instance: TextInput | null) => void' is not assignable to type '((((instance: TextInput | null) => void) | RefObject<TextInput>) & (((instance: PropsWithChildren<InputProps> | null) => void) | RefObject<...>)) | null | undefined'.
-    Type '(instance: TextInput | null) => void' is not assignable to type '((instance: TextInput | null) => void) & ((instance: PropsWithChildren<InputProps> | null) => void)'.
-      Type '(instance: TextInput | null) => void' is not assignable to type '(instance: PropsWithChildren<InputProps> | null) => void'.
-        Types of parameters 'instance' and 'instance' are incompatible.
-          Type 'PropsWithChildren<InputProps> | null' is not assignable to type 'TextInput | null'.
-            Type 'PropsWithChildren<InputProps>' is not assignable to type 'TextInput | null'.
-             ref={ref}
- */
-
-import React, {useCallback} from 'react';
+import React, {PropsWithChildren, useCallback} from 'react';
 import {
   NativeSyntheticEvent,
   StyleSheet,
@@ -29,47 +15,48 @@ export type MyTextInputProps = InputProps & {
   nextRef?: FocusRef; // reference to where the "next" key will change focus to
 };
 
-export const MyTextInput = React.forwardRef<TextInput, MyTextInputProps>(
-  (props, ref) => {
-    const focusNextRef = useCallback(
-      () => props.nextRef?.current && props.nextRef.current.focus(),
-      [props.nextRef],
-    );
-    const submitEditing = useCallback(
-      (e: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => {
-        if (props.onSubmitEditing !== undefined) {
-          props.onSubmitEditing(e);
-        }
-        focusNextRef();
-      },
-      [props, focusNextRef],
-    );
-
-    const baseInputProps = useCallback(() => {
-      const baseProps = {...props};
-
-      if (baseProps.returnKeyType === undefined) {
-        if (baseProps.nextRef !== undefined) {
-          baseProps.returnKeyType = 'next';
-        } else if (props.onSubmitEditing !== undefined) {
-          baseProps.returnKeyType = 'done';
-        }
+export const MyTextInput = React.forwardRef<
+  TextInput,
+  PropsWithChildren<MyTextInputProps>
+>((props, ref) => {
+  const focusNextRef = useCallback(
+    () => props.nextRef?.current && props.nextRef.current.focus(),
+    [props.nextRef],
+  );
+  const submitEditing = useCallback(
+    (e: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => {
+      if (props.onSubmitEditing !== undefined) {
+        props.onSubmitEditing(e);
       }
+      focusNextRef();
+    },
+    [props, focusNextRef],
+  );
 
-      return baseProps;
-    }, [props]);
+  const baseInputProps = useCallback(() => {
+    const baseProps = {...props};
 
-    return (
-      <View style={styles.inputWrapper}>
-        <Input
-          ref={ref}
-          onSubmitEditing={submitEditing}
-          {...baseInputProps()}
-        />
-      </View>
-    );
-  },
-);
+    if (baseProps.returnKeyType === undefined) {
+      if (baseProps.nextRef !== undefined) {
+        baseProps.returnKeyType = 'next';
+      } else if (props.onSubmitEditing !== undefined) {
+        baseProps.returnKeyType = 'done';
+      }
+    }
+
+    return baseProps;
+  }, [props]);
+
+  return (
+    <View style={styles.inputWrapper}>
+      <Input
+        {...baseInputProps()}
+        ref={ref as React.Ref<PropsWithChildren<TextInput>>}
+        onSubmitEditing={submitEditing}
+      />
+    </View>
+  );
+});
 
 const styles = StyleSheet.create({
   inputWrapper: {
