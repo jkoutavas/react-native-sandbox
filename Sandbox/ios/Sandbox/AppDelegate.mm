f958a4b573d28c4646f889f7de86beb38dc98ae8
Jay Koutavas <jay@heynow.com>
2024-07-03 10:50:25 -0400

    Don't initialize Firebase on iOS

--- Sandbox/ios/Sandbox/AppDelegate.mm
+++ Sandbox/ios/Sandbox/AppDelegate.mm
@@ -13,7 +13,7 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  [FIRApp configure];
+  //[FIRApp configure];
   
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
