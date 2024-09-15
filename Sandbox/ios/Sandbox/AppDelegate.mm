d0cb335e91538a54c30e5f0ff3d69f6309173136
jay koutavas <jay@heynow.com>
2024-09-14 23:19:02 -0400

    Upgraded to RN 0.75.3

--- Sandbox/ios/Sandbox/AppDelegate.mm
+++ Sandbox/ios/Sandbox/AppDelegate.mm
@@ -18,7 +18,12 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+	- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
+}
+ 
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
